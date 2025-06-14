01035016859d6e41d37f0e63258f5855d6639406
SakaGintoki <108750245+SakaGintoki@users.noreply.github.com>
2025-06-14 17:57:02 +0700

    Update 2025_06_11_000005_create_pesanan_table.php
    Update Database kolom total

--- backend/laravel-api/database/migrations/2025_06_11_000005_create_pesanan_table.php
+++ backend/laravel-api/database/migrations/2025_06_11_000005_create_pesanan_table.php
@@ -7,18 +7,18 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void {
         Schema::create('pesanan', function (Blueprint $table) {
-    $table->id('id_pesanan');
-    $table->unsignedBigInteger('id_pembeli');
-    $table->date('tanggal_pesanan');
-    $table->string('status', 50);
-    $table->timestamps();
+            $table->id('id_pesanan');
+            $table->unsignedBigInteger('id_pembeli')->nullable();
+            $table->date('tanggal_pesanan');
+            $table->string('status', 50)->default('Pending');
+            $table->decimal('total', 10, 2); // ← Tambahkan kolom total
+            $table->timestamps();
 
-    $table->foreign('id_pembeli')
-        ->references('id_pembeli')->on('pembeli')
-        ->onUpdate('cascade')
-        ->onDelete('restrict');
-});
-
+            $table->foreign('id_pembeli')
+                ->references('id_pembeli')->on('pembeli')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
     }
 
     public function down(): void {
